c34f5c873a4fb2e6a8dfade83f8c082809c29678
estigma88 <estigma88@gmail.com>
2018-01-18 17:47:11 -0500

    Adding chord UTs

--- chord/src/test/java/co/edu/uniquindio/chord/node/ChordNodeTest.java
+++ chord/src/test/java/co/edu/uniquindio/chord/node/ChordNodeTest.java
@@ -47,7 +47,6 @@ public class ChordNodeTest {
     @Test
     public void lookUp_isBetweenRightIncluded_successor() {
         Key id = mock(Key.class);
-        Key next = mock(Key.class);
 
         when(id.isBetweenRightIncluded(key, successor)).thenReturn(true);
 
@@ -79,5 +78,109 @@ public class ChordNodeTest {
                 .getStringHashing());
         assertThat(messageCaptor.getValue().getParam(Protocol.LookupParams.TYPE.name())).isEqualTo(LookupType.LOOKUP.name());
         assertThat(lookUpKey).isEqualTo(result);
+    }
+
+    @Test
+    public void createRing_create_initializeSuccessors() {
+        chordNode.createRing();
+
+        assertThat(chordNode.getPredecessor()).isNull();
+        assertThat(chordNode.getSuccessor()).isEqualTo(chordNode.getKey());
+
+        verify(successorList).initializeSuccessors();
+    }
+
+    @Test
+    public void join_sendMessage_initializeSuccessors() {
+        Key node = mock(Key.class);
+
+        Key successorResult = mock(Key.class);
+
+        when(communicationManager.sendMessageUnicast(anyObject(),
+                eq(Key.class), eq(Protocol.LookupResponseParams.NODE_FIND.name()))).thenReturn(successorResult);
+
+        chordNode.join(node);
+
+        verify(communicationManager).sendMessageUnicast(messageCaptor.capture(),
+                eq(Key.class), eq(Protocol.LookupResponseParams.NODE_FIND.name()));
+
+        assertThat(chordNode.getPredecessor()).isNull();
+        assertThat(chordNode.getSuccessor()).isEqualTo(successorResult);
+        assertThat(messageCaptor.getValue().getMessageType()).isEqualTo(Protocol.LOOKUP);
+        assertThat(messageCaptor.getValue().getAddress().getDestination()).isEqualTo(node.getValue());
+        assertThat(messageCaptor.getValue().getAddress().getSource()).isEqualTo(key.getValue());
+        assertThat(messageCaptor.getValue().getParam(Protocol.LookupParams.HASHING.name())).isEqualTo(key
+                .getStringHashing());
+        assertThat(messageCaptor.getValue().getParam(Protocol.LookupParams.TYPE.name())).isEqualTo(LookupType.JOIN.name());
+
+        verify(successorList).initializeSuccessors();
+    }
+
+    @Test
+    public void notify_predecessorNotNullAndIsNotBetween_notNotify(){
+        Key node = mock(Key.class);
+
+        when(node.isBetween(predecessor, key)).thenReturn(false);
+
+        chordNode.notify(node);
+
+        verifyNoMoreInteractions(observable);
+    }
+
+    @Test
+    public void notify_predecessorIsNullAndNodeEqualKey_predecessorNull(){
+        Key node = key;
+
+        chordNode = new ChordNode(communicationManager, successor, null, fingersTable, successorList, key, observable);
+
+        chordNode.notify(node);
+
+        assertThat(chordNode.getPredecessor()).isNull();
+    }
+
+    @Test
+    public void notify_predecessorIsNullIsBetweenAndNodeEqualKey_predecessorNull(){
+        Key node = key;
+
+        when(node.isBetween(predecessor, key)).thenReturn(true);
+
+        chordNode.notify(node);
+
+        assertThat(chordNode.getPredecessor()).isNull();
+    }
+
+    @Test
+    public void notify_predecessorIsNullAndNodeNotEqualKey_predecessorNull(){
+        Key node = mock(Key.class);
+
+        chordNode = new ChordNode(communicationManager, successor, null, fingersTable, successorList, key, observable);
+
+        String[] message = new String[2];
+        message[0] = "REASSIGN";
+        message[1] = null;
+
+        chordNode.notify(node);
+
+        assertThat(chordNode.getPredecessor()).isEqualTo(node);
+
+        verify(observable).notifyMessage(message);
+    }
+
+    @Test
+    public void notify_predecessorIsNotNullAndNodeNotEqualKey_predecessorNull(){
+        Key node = mock(Key.class);
+
+        String[] message = new String[2];
+        message[0] = "REASSIGN";
+        message[1] = "hashPredecessor";
+
+        when(node.isBetween(predecessor, key)).thenReturn(true);
+        when(node.getValue()).thenReturn("hashPredecessor");
+
+        chordNode.notify(node);
+
+        assertThat(chordNode.getPredecessor()).isEqualTo(node);
+
+        verify(observable).notifyMessage(message);
     }
 }
