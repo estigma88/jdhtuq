effffda17d24f5b1c120d22d022c97da77252bc0
estigma88 <estigma88@gmail.com>
2018-02-01 12:11:25 -0500

    Adding chord UTs

--- chord/src/test/java/co/edu/uniquindio/chord/node/FingersTableTest.java
+++ chord/src/test/java/co/edu/uniquindio/chord/node/FingersTableTest.java
@@ -7,6 +7,8 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
+import java.math.BigInteger;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
@@ -37,6 +39,17 @@ public class FingersTableTest {
 
     @Test
     public void findClosestPresedingNode_notFound_returnNodeKey() {
+        Key search = mock(Key.class);
+
+        Key result = fingersTable.findClosestPresedingNode(search);
+
+        assertThat(result).isEqualTo(key);
+    }
+
+    @Test
+    public void findClosestPresedingNode_notFoundNullFingers_returnNodeKey() {
+        fingersTable = spy(new FingersTable(new Key[]{null, null, null}, chordNode, 0, 3));
+
         Key search = mock(Key.class);
 
         Key result = fingersTable.findClosestPresedingNode(search);
@@ -97,5 +110,38 @@ public class FingersTableTest {
         fingersTable.fixFingers();
 
         assertThat(fingersTable.getFingersTable()[0]).isEqualTo(successor);
+    }
+
+    @Test
+    public void setSuccessor_set_newFirstFinger() {
+        Key newSuccessor = mock(Key.class);
+
+        fingersTable.setSuccessor(newSuccessor);
+
+        assertThat(fingersTable.getFingersTable()[0]).isEqualTo(newSuccessor);
+    }
+
+    @Test
+    public void createNext_next0_newKey() {
+        Key key = mock(Key.class);
+
+        when(key.getHashing()).thenReturn(new BigInteger("10"));
+
+        Key next = fingersTable.createNext(key);
+
+        assertThat(next.getHashing()).isEqualTo(new BigInteger("3"));
+    }
+
+    @Test
+    public void createNext_next2_newKey() {
+        fingersTable = spy(new FingersTable(fingersTableArray, chordNode, 2, 3));
+
+        Key key = mock(Key.class);
+
+        when(key.getHashing()).thenReturn(new BigInteger("10"));
+
+        Key next = fingersTable.createNext(key);
+
+        assertThat(next.getHashing()).isEqualTo(new BigInteger("6"));
     }
 }
