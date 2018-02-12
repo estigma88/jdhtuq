69bbaf600ee2f50b8beab0b0eb8794ba22589691
daniel <estigma88@gmail.com>
2018-02-11 20:13:13 -0500

    Adding spring boot to startup the java swing app

--- main/desktop-gui/src/main/java/co/edu/uniquindio/dht/Main.java
+++ main/desktop-gui/src/main/java/co/edu/uniquindio/dht/Main.java
@@ -1,59 +1,30 @@
 package co.edu.uniquindio.dht;
 
 import co.edu.uniquindio.dht.gui.MainFrame;
-import org.apache.log4j.xml.DOMConfigurator;
+import org.apache.log4j.Logger;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 import javax.swing.*;
-import javax.xml.parsers.FactoryConfigurationError;
-import java.io.File;
-import java.net.URISyntaxException;
-import java.security.CodeSource;
+import java.awt.*;
 
+@SpringBootApplication
 public class Main {
-    /**
-     * Path for properties fiel
-     */
-    private static final String PROPERTIES = "resources/logger.xml";
-
     public static void main(String[] args) {
-        try {
-            loadLoggerProperties();
+        ConfigurableApplicationContext applicationContext = new SpringApplicationBuilder(Main.class)
+                .headless(false).run(args);
 
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (UnsupportedLookAndFeelException e) {
-
-        } catch (ClassNotFoundException e) {
-
-        } catch (InstantiationException e) {
-
-        } catch (IllegalAccessException e) {
-
-        }
-
-        MainFrame mainFrame = new MainFrame();
-        mainFrame.setVisible(true);
+        EventQueue.invokeLater(() -> {
+            MainFrame mainFrame = applicationContext.getBean(MainFrame.class);
+            mainFrame.setVisible(true);
+        });
     }
 
-    private static void loadLoggerProperties() throws FactoryConfigurationError {
-        try {
-            CodeSource codeSource = Main.class.getProtectionDomain()
-                    .getCodeSource();
-            File jarFile;
-
-            jarFile = new File(codeSource.getLocation().toURI().getPath());
-
-            File jarDir = jarFile.getParentFile();
-
-            File propFile = null;
-            if (jarDir != null && jarDir.isDirectory()) {
-                propFile = new File(jarDir, PROPERTIES);
-            }
-
-            DOMConfigurator.configure(propFile.getPath());
-
-        } catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
+    @Bean
+    public MainFrame mainFrame() {
+        return new MainFrame();
     }
 }
 
