2a225e3bf04bea51cc783875b08af0b17d4ff897
daniel <estigma88@gmail.com>
2018-03-11 12:15:53 -0500

    Adding integration test for data structure

--- it/data-structure-it/src/test/java/co/edu/uniquindio/dht/it/datastructure/CucumberIntegrationIT.java
+++ it/data-structure-it/src/test/java/co/edu/uniquindio/dht/it/datastructure/CucumberIntegrationIT.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/resources/features"}, format = {"pretty", "html:target/reports/cucumber/html",
-        "json:target/cucumber.json", "usage:target/usage.jsonx", "junit:target/junit.xml"})
+@CucumberOptions(features = {"src/test/resources/features"}, format = {"pretty", "html:out/reports/cucumber/html",
+        "json:out/cucumber.json", "usage:out/usage.jsonx", "junit:out/junit.xml"})
 public class CucumberIntegrationIT {
 }
