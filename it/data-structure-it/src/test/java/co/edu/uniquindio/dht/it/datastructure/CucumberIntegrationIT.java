100452e02a4c0835166b9e772b11abf7673dfcee
daniel <estigma88@gmail.com>
2018-04-08 21:28:27 -0500

    MOvving files

---
+++ it/data-structure-it/src/test/java/co/edu/uniquindio/dht/it/datastructure/CucumberIntegrationIT.java
@@ -0,0 +1,11 @@
+package co.edu.uniquindio.dht.it.datastructure;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/resources/features"}, format = {"pretty", "html:out/reports/cucumber/html",
+        "json:out/cucumber.json", "usage:out/usage.jsonx", "junit:out/junit.xml"}, tags = {"~@enable"})
+public class CucumberIntegrationIT {
+}
