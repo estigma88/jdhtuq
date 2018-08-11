491d71e3bacd75683e26e620e0d60b18acabea8f
daniel <estigma88@gmail.com>
2018-08-11 17:24:17 -0500

    Improvent tests

--- it/socket-it/src/ittest/java/co/edu/uniquindio/dht/it/socket/test/CucumberIntegrationIT.java
+++ it/socket-it/src/ittest/java/co/edu/uniquindio/dht/it/socket/test/CucumberIntegrationIT.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/ittest/resources/features"}, format = {"pretty", "html:build/reports/cucumber/html",
+@CucumberOptions(format = {"pretty", "html:build/reports/cucumber/html",
         "json:build/cucumber.json", "usage:build/usage.jsonx", "junit:build/junit.xml"}, tags = {"~@enable"})
 public class CucumberIntegrationIT {
 
